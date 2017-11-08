724f79ea849a436852d53976ea91a42e36f1173a
Jean Sanchez <jean.sanchez@intersys.mx>
2017-11-08 17:43:38 -0600

    quitar vista prueba

--- form_lobby/form_lobby/views.py
+++ form_lobby/form_lobby/views.py
@@ -17,26 +17,10 @@ def lobby(request):
             asd = form.save()
 
             return HttpResponseRedirect('/gracias/')
+        else:
+            print "NO"
     else:
         form = LobbyForm()
-
-    return render(request, 'lobby_form.html', {'form': form})
-
-def get_name(request):
-    # if this is a POST request we need to process the form data
-    if request.method == 'POST':
-        # create a form instance and populate it with data from the request:
-        form = NameForm(request.POST)
-        # check whether it's valid:
-        if form.is_valid():
-            # process the data in form.cleaned_data as required
-            # ...
-            # redirect to a new URL:
-            return HttpResponseRedirect('/gracias/')
-
-    # if a GET (or any other method) we'll create a blank form
-    else:
-        form = NameForm()
 
     return render(request, 'lobby_form.html', {'form': form})
 
