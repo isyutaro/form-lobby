1bb032e858f97a9e6570ea67e18e8d5cca86efbd
Jean Sanchez <jean.sanchez@intersys.mx>
2017-11-08 20:19:27 -0600

    sin CSS main y con primera version de bootstrap

--- form_lobby/form_lobby/settings/development.py
+++ form_lobby/form_lobby/settings/development.py
@@ -2,3 +2,5 @@ from common import *
 
 # Emails aren't send but displayed in console
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+ALLOWED_HOSTS = ['10.0.10.14', '10.0.10.14.xip.io',]
